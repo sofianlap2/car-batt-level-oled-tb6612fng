90c06531895bd2145ccdb98b5a5b344330fdbe2c
sofiane nabli <76880198+sofianlap2@users.noreply.github.com>
2026-04-10 07:45:43 +0100

    Add transmitter-nopwm.ino for RF control

---
+++ transmitter-nopwm.ino
@@ -0,0 +1,108 @@
+#include <SPI.h>
+#include <RF24.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// OLED
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// RF
+RF24 radio(9, 10);
+const byte txAddr[6] = "NODE1";
+const byte rxAddr[6] = "NODE2";
+
+// DATA
+struct TXData {
+  int speed;
+  int turn;
+  bool lights;
+  bool horn;
+};
+
+struct RXData {
+  float battery;
+  int signal;
+};
+
+TXData txData;
+RXData rxData;
+
+// JOYSTICK
+const int joyX = A1;
+const int joyY = A0;
+const int joySW = 3;
+
+// SWITCH
+const int switchPin = 2;
+
+unsigned long lastReceiveTime = 0;
+
+void setup() {
+  pinMode(switchPin, INPUT_PULLUP);
+  pinMode(joySW, INPUT_PULLUP);
+
+  Wire.begin();
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+
+  radio.begin();
+  radio.setChannel(108);
+  radio.setDataRate(RF24_250KBPS);
+
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setRetries(5, 15);
+  radio.enableAckPayload();
+  radio.enableDynamicPayloads();
+
+  radio.openWritingPipe(txAddr);
+  radio.openReadingPipe(1, rxAddr);
+
+  radio.stopListening();
+}
+
+void loop() {
+  int x = analogRead(joyX);
+  int y = analogRead(joyY);
+
+  // DEAD ZONE
+  if (x > 480 && x < 540) x = 512;
+  if (y > 480 && y < 540) y = 512;
+
+  txData.speed = map(y, 0, 1023, -255, 255);
+  txData.turn = map(x, 0, 1023, -255, 255);
+
+  txData.lights = (digitalRead(switchPin) == LOW);
+  txData.horn = (digitalRead(joySW) == LOW);
+
+  bool ok = radio.write(&txData, sizeof(txData));
+
+  if (ok && radio.isAckPayloadAvailable()) {
+    radio.read(&rxData, sizeof(rxData));
+    lastReceiveTime = millis();
+  }
+
+  bool signalLost = (millis() - lastReceiveTime > 500);
+
+  display.clearDisplay();
+
+  display.setCursor(0, 0);
+  display.print("Battery: ");
+  display.print(rxData.battery, 2);
+  display.println("V");
+
+  display.setCursor(0, 20);
+  display.print("Signal: ");
+  display.println((ok && !signalLost) ? "OK" : "Lost");
+
+  display.setCursor(0, 40);
+  display.print("Horn: ");
+  display.println(txData.horn ? "ON" : "OFF");
+
+  display.display();
+
+  delay(50);
+}
